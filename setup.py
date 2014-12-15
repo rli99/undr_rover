5be28967171664dfaaefef135ebfa5f742195bec
Roger Li <r.li3@student.unimelb.edu.au>
2014-12-15 16:54:05 +1100

    Pip testing.

--- setup.py
+++ setup.py
@@ -8,17 +8,17 @@ setup(
     author='Roger Li',
     author_email='r.li3@student.unimelb.edu.au',
     packages=['undr_rover'],
-    entry_points={
-        'console_scripts':['undr_rover = undr_rover.undr_rover:main']
-    },
+    #entry_points={
+    #    'console_scripts':['undr_rover = undr_rover.undr_rover:main']
+    #},
     url='https://github.com/rli99/undr_rover',
     license='LICENSE.txt',
     description=(
         'UNDR ROVER: Unmapped primer directed read overlap variant caller.'),
     long_description=open('README.txt').read(),
-    install_requires=[
-        "PyVCF",
-        "biopython",
-        "pyfaidx"
-    ],
+    #install_requires=[
+    #    "PyVCF",
+    #    "biopython",
+    #    "pyfaidx"
+    #],
 )
